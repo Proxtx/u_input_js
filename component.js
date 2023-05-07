7fe06bcbf462d713c04c465962b27320a5bebe7a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-07 23:39:08 +0200

    working

---
+++ component.js
@@ -0,0 +1,33 @@
+export class Component {
+  constructor(options) {
+    this.document = options.shadowDom;
+    this.arg1 = this.document.getElementById("arg1");
+    this.initPromise = this.init();
+  }
+
+  async init() {
+    await uiBuilder.ready(this.arg1);
+    this.arg1.component.inputDefinition({
+      type: "text",
+      name: "Code",
+      placeholder: "Code",
+    });
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  async setValue(value) {
+    await this.initPromise;
+    await this.arg1.component.setValue(value.code);
+  }
+
+  async getValue() {
+    return {
+      evaluate: true,
+      code: await this.arg1.component.getValue(),
+      pwd: this.config.pwd,
+    };
+  }
+}
