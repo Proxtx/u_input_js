65d08404d8c16cf7fb6d0d1a7f0f18a362df1b4f
Proxtx <gopro54122@gmail.com>
2023-05-07 22:03:17 +0000

    Add API access for eval

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-import { input } from "../../public/meta.js";
+import { input, api } from "../../public/meta.js";
 
 export const evaluate = async (value) => {
   return await eval(await input.resolveArgument(value.pwd, value.code));
